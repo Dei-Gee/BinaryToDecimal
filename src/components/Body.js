2fb612413cff90e1b945c5f1ac01476a87d0f9b3
Dei-Gee <deji2109@gmail.com>
2020-04-24 19:27:10 -0700

    styling done

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Header from './Header';
 import { binaryValidator, binaryToDecimal } from '../utils/utils';
+import '../styles/Body.css';
 
 const Body = () => {
     const [binary, setBinary] = useState(0);
@@ -9,9 +10,11 @@ const Body = () => {
     return(
         <div>
             <Header />
-            <input type="text" onChange={(e) => {setBinary(e.target.value)}} maxLength="8" />
-            <button type="button" onClick={() => {setDecimal(binaryToDecimal(binary))}}>Convert to Decimal</button>
-            <h3>{decimal}</h3>
+            <main className="main-wrapper">
+                <input className="textbox" type="text" onChange={(e) => {setBinary(e.target.value)}} maxLength="8" />
+                <button className="convert-button" type="button" onClick={() => {setDecimal(binaryToDecimal(binary))}}>Convert to Decimal</button>
+                <h3 className="result-display">{decimal}</h3>
+            </main>
         </div>
     );
 }
